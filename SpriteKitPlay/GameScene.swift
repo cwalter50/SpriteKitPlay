1b25759073b43942ac855d345bf256f55d3af23e
Chris Walter <walt50@gmail.com>
2021-01-23 15:07:30 -0500

    added touches to the hammer

--- SpriteKitPlay/GameScene.swift
+++ SpriteKitPlay/GameScene.swift
@@ -12,6 +12,8 @@ class GameScene: SKScene {
 
     var background = SKSpriteNode(imageNamed: "Background")
     
+    var thorsHammer = SKSpriteNode()
+    
     override func didMove(to view: SKView)
     {
         background.position = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
@@ -24,5 +26,37 @@ class GameScene: SKScene {
         
         physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
         
+        thorsHammer = childNode(withName: "Hammer") as! SKSpriteNode
+        thorsHammer.position = CGPoint(x: 200, y: 200)
+        
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+  
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        print(location)
+        let move = SKAction.moveTo(x: location.x, duration: 0.2)
+        let movey = SKAction.moveTo(y: location.y, duration: 0)
+        
+
+        thorsHammer.run(move)
+        thorsHammer.run(movey)
+//        thorsHammer.position = location
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+   
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        print(location)
+        let move = SKAction.moveTo(x: location.x, duration: 0.2)
+        let movey = SKAction.moveTo(y: location.y, duration: 0)
+        
+
+        thorsHammer.run(move)
+        thorsHammer.run(movey)
+    }
+    
+    
 }
